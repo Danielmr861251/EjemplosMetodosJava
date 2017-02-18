9f5d0c7d9971e088ad753c9ca7de418f2b522c7c
Danielmr861251 <162t0314@itsm.edu.mx>
2017-02-18 10:08:57 -0600

    Create metodo3_uno.java

---
+++ metodo3_uno.java
@@ -0,0 +1,17 @@
+public class metodo3_uno {
+
+	public static void main(String[] args) {
+		// con retorno y sin parametro
+		
+		int suma = sumas();
+		System.out.println("la suma es: "+suma);
+	
+	}public static int sumas(){
+		
+		int a=2,b=3;
+		int sum = a+b;
+		return sum;
+		
+	}
+
+}
