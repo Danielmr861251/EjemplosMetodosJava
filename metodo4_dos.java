f5d39983848784cee88e8d8601921dfa52b2577b
Danielmr861251 <162t0314@itsm.edu.mx>
2017-02-18 10:15:13 -0600

    Create metodo4_dos.java

---
+++ metodo4_dos.java
@@ -0,0 +1,17 @@
+public class metodo4_dos {
+
+	public static void main(String[] args) {
+		// con retorno y con parametros
+		int a=2,b=100;
+		int resul = multi(a,b);
+		System.out.println("el producto es: "+resul);
+		
+		
+		
+	}public static int multi(int a, int b){
+		int pro = a*b;
+		return pro;
+		
+	}
+
+}
