dcd4e994b60f46a106b409daee519a65c0f2ff5b
Danielmr861251 <162t0314@itsm.edu.mx>
2017-02-18 10:10:54 -0600

    Create metodo3_tres.java

---
+++ metodo3_tres.java
@@ -0,0 +1,15 @@
+public class metodo3_tres {
+
+	public static void main(String[] args) {
+		// con retorno y sin parametros
+		int divi=dividir();
+		System.out.println("la division es: "+divi);
+		
+	}public static int dividir(){
+		int a=6,b=2;
+		int div = a/b;
+		return div;
+		
+	}
+
+}
