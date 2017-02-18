cacd8408ba21782f6b910ba1f8af47781eb3d9ec
Danielmr861251 <162t0314@itsm.edu.mx>
2017-02-18 10:13:37 -0600

    Create metodo3_cinco.java

---
+++ metodo3_cinco.java
@@ -0,0 +1,14 @@
+public class metodo3_cinco {
+
+	public static void main(String[] args) {
+		// con retorno y sin parametros
+		int res=operacion();
+		System.out.println("el resultado es: "+res);
+	}public static int operacion(){
+		
+		int a=4,b=10,c=10;
+		int op = b-a*c;
+		return op;
+	}
+
+}
