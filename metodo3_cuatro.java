099e91d5c4e541ae7f294b7f7be92786257c7952
Danielmr861251 <162t0314@itsm.edu.mx>
2017-02-18 10:11:29 -0600

    Create metodo3_cuatro.java

---
+++ metodo3_cuatro.java
@@ -0,0 +1,17 @@
+public class metodo3_cuatro {
+
+	public static void main(String[] args) {
+		// con retorno y sin parametros
+		
+		int resta = restar();
+		System.out.println("la resta es: "+resta);
+		
+
+	}public static int restar(){
+		int x=100000,b=20000;
+		int res = x-b;
+		return res;
+		
+	}
+
+}
