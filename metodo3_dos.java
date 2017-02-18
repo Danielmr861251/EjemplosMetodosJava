a191bffc0e21578090327168ef76902dc4c3b7be
Danielmr861251 <162t0314@itsm.edu.mx>
2017-02-18 10:09:51 -0600

    Create metodo3_dos.java

---
+++ metodo3_dos.java
@@ -0,0 +1,15 @@
+public class metodo3_dos {
+
+	public static void main(String[] args) {
+		// con retorno y sin parametros
+		int multi=multiplicar();
+		System.out.println("la multiplicacion es: "+multi);
+		
+	}public static int multiplicar(){
+		int a=2,b=3;
+		int res = a*b;
+		return res;
+		
+	}
+
+}
